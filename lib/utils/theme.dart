6d37881bc2ce152c8d8174eeb0fa543ea62fdc2f
Melvyn <melvyn.brochars@gmail.com>
2024-12-20 15:26:30 +0100

    Added font family in app theme

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,23 +1,7 @@
 import 'package:flutter/material.dart';
-
-const mainColor = Color(0x000000); // Netflix Red
+import 'package:google_fonts/google_fonts.dart';
 
 final ThemeData appTheme = ThemeData(
-  fontFamily: 'Netflix Sans, Helvetica Neue, Segoe UI, Roboto, Ubuntu, sans-serif',
-  colorScheme: ColorScheme.fromSwatch().copyWith(
-    primary: mainColor,
-    secondary: Colors.black,
-  ),
   scaffoldBackgroundColor: Colors.black,
-  textTheme: const TextTheme(
-    displayLarge: TextStyle(
-      fontSize: 32.0,
-      fontWeight: FontWeight.bold,
-      color: Colors.white,
-    ), // Use for large headers
-    bodyLarge: TextStyle(
-      fontSize: 16.0,
-      color: Colors.white,
-    ), // Use for general text
-  ),
+  textTheme: GoogleFonts.robotoTextTheme(),
 );
