31d7022215b85db9bce22ec9890158415e3590ec
Melvyn <melvyn.brochars@gmail.com>
2025-01-07 19:25:50 +0100

    DEV_MODE variable added to skip login form when developping ( ignore in production env )

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toudoum/screens/home.dart';
 import 'main_page.dart';
 import '../utils/localization.dart';
 
@@ -12,17 +13,33 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  // lib/utils/dev_mode.dart
+  int DEV_MODE = 0; // Set to 1 for development mode (bypasses login)
+
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => MainPage(localization: widget.localization),
-        ),
-      );
-    });
+    // If DEV_MODE is 1, navigate directly to HomePage
+    if (DEV_MODE == 1) {
+      Future.delayed(const Duration(seconds: 1), () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(),
+          ),
+        );
+      });
+    } else {
+      // If not in DEV_MODE, wait 3 seconds and go to the login screen
+      Future.delayed(const Duration(seconds: 3), () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MainPage(localization: widget.localization),
+          ),
+        );
+      });
+    }
   }
 
   @override
